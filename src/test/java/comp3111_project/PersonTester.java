4b34c8fecc71d4ec7e81e6a8b192cf40a17ec5f6
Henry Lam <hcylam@connect.ust.hk>
2022-11-18 20:02:52 +0800

    Adding javadoc comments into the testers.

--- src/test/java/comp3111_project/PersonTester.java
+++ src/test/java/comp3111_project/PersonTester.java
@@ -9,14 +9,23 @@ import org.junit.Test;
 import org.junit.Before;
 import org.junit.Test;
 
+
+/**
+ * Thie class is for junit to run tests on.
+ * This specifically tests the person class.
+ * @author Henry
+ */
 public class PersonTester {
+	// this creates an instance of Person to test the constructor and its accessor functions on.
 	Person test;
 	
+	// no setup required.
 	@Before
 	public void setup() throws Exception {
 		
 	}
 	
+	// this will test the person class' constructor and all of its accessor functions given correct inputs.
 	@Test
 	public void testWithCorrectInputs() {
 		test = new Person("20723854", "Henry Chun Yin Lam", "hcylam@connect.ust.hk", "100", "100", "0", "1", "1", "test");
@@ -31,6 +40,7 @@ public class PersonTester {
 		System.out.print(test.getConcerns());
 	}
 	
+	// this will test the person class' constructor and all of its accessor functions given correct inputs, slightly different inputs from last time.
 	@Test
 	public void testWithCorrectInputs2() {
 		test = new Person("20723854", "Henry Chun Yin Lam", "hcylam@connect.ust.hk", "100", "100", "1", "0", "0", "test");
@@ -45,6 +55,7 @@ public class PersonTester {
 		System.out.print(test.getConcerns());
 	}
 	
+	// this will test the person class' constructor if given incorrect inputs in the areas that require numberical input for it to be parsed.
 	@Test
 	public void testwithIncorrectInputs() {
 		test = new Person("20723854", "Henry Chun Yin Lam", "hcylam@connect.ust.hk", "wrong", "incorrect", "words", "test", "one", "test");
--- src/test/java/comp3111_project/StatisticsTester.java
+++ src/test/java/comp3111_project/StatisticsTester.java
@@ -3,14 +3,24 @@ package comp3111_project;
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * This class for junit to run tests on.
+ * This specifically tests the Statistics class.
+ * @author Henry
+ */
 public class StatisticsTester {
+	// creates an instance of Statistics to run tests on.
 	Statistics test;
 	
+	// no setup required.
 	@Before
 	public void setup() throws Exception {
 		
 	}
 	
+	// this will test statistics given correct inputs and its accessor and setter functions.
+	// tests without the correct input are not required because statistics specifies strings as the input.
+	// since the strings are not parsed, this means that all inputs must be correct, and therefore no other tests are required.
 	@Test
 	public void testWithCorrectInputs() {
 		test = new Statistics("test", "test");
